7d688333ad561372f663194fd04e9cf0a51425f8
David Akpan <daviduwana@gmail.com>
2020-02-25 16:18:56 +0100

    Updates

---
+++ precache-manifest.ca75b977e2c9becf96c2952ad5ee396b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e796c1545f17a74e76bdbf07d55fca5b",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "06d5f4981ad02c620f39",
+    "url": "/random-quote-machine/static/css/main.815c3195.chunk.css"
+  },
+  {
+    "revision": "af5ab42af7dc6a30e714",
+    "url": "/random-quote-machine/static/js/2.9aca466c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-quote-machine/static/js/2.9aca466c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d5f4981ad02c620f39",
+    "url": "/random-quote-machine/static/js/main.2b90156a.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
