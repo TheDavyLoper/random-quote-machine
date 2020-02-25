71eb267c52fc803ab9b4a796cfa9038b3d42f44c
David Akpan <daviduwana@gmail.com>
2020-02-25 16:14:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quote: '',
+      author: ''
+    }
+    this.quoteGen = this.quoteGen.bind(this)
+  }
+  
+  componentDidMount() {
+     this.quoteGen()
+  }
+
+  quoteGen() {
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then(res =>  res.json())
+    .then(data => {
+      let arrNum = Math.floor(Math.random() * data.quotes.length);
+      this.setState({
+        quote: data.quotes[arrNum].quote,
+        author: data.quotes[arrNum].author
+      })
+    })
+    .catch(error => console.log(error))
+  }
+ 
+  
+  render() {
+    console.log(this.state.quote)
+    return (
+    <div id="quote-box">
+        <div id="text"><span><i class="fa fa-quote-left"> </i></span> {this.state.quote}</div>
+        <div id="author">- {this.state.author}</div>
+        <div className="flex">
+          <div id="tweetLink"><a href="twitter.com/intent/tweet" id="tweet-quote"><i class="fab fa-twitter"></i></a></div>
+          <button id="new-quote" onClick={this.quoteGen}>New Quote</button>
+        </div>
+        
     </div>
-  );
+    )
+  }
 }
 
 export default App;
